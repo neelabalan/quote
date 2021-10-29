7d13caf42d484a92658fc994b5752dfe3c89a117
neelabalan <neelabalan.n@outlook.com>
2021-10-29 21:28:05 +0530

    changes for new jsondb

--- quote.py
+++ quote.py
@@ -9,18 +9,19 @@ import tempfile
 import string
 from typing import Optional
 
-JSONEXT = '.json'
-TOMLEXT = '.toml'
-command = string.Template('$editor $filename')
+from jsondb import jsondb
+from jsondb import DuplicateEntryError
 
-
-import jsondb
-
+import typer
 import toml
 from rich.console import Console
 from rich.panel import Panel
 
-import typer
+
+JSONEXT = ".json"
+TOMLEXT = ".toml"
+command = string.Template("$editor $filename")
+
 
 app = typer.Typer()
 console = Console()
@@ -29,23 +30,15 @@ color = "white"
 columns = shutil.get_terminal_size().columns - 6
 
 # template
-'''
+"""
 [[-]]
 quote = ''
 author = ''
 reference = ''
 tags = []
-'''
-quotes_template = {
-	'-': [
-		{
-			'quote': '',
-			'author': '',
-			'reference': '',
-			'tags': []
-		}
-	]
-}
+"""
+quotes_template = {"-": [{"quote": "", "author": "", "reference": "", "tags": []}]}
+
 
 def display_quote(quote):
     console.print(
@@ -60,60 +53,70 @@ def display_quote(quote):
         style=color,
     )
 
-def environ_present(key='EDITOR'):
-	return key in os.environ
+
+def environ_present(key="EDITOR"):
+    return key in os.environ
+
 
 def open_temp_toml_file():
-	if environ_present('EDITOR'):
-		editor = os.environ['EDITOR']
-		fd, filename = tempfile.mkstemp(suffix=TOMLEXT, text=True)
-		with open(filename, 'w') as file:
-			toml.dump(quotes_template, file)
-		write_status = subprocess.call(
-			command.substitute(editor=editor, filename=filename), 
-			shell=True
-		)
-		if write_status != 0:
-			os.remove(filename)
-		return filename, write_status
-	else:
-		raise Exception('EDITOR not found in env')
+    if environ_present("EDITOR"):
+        editor = os.environ["EDITOR"]
+        fd, filename = tempfile.mkstemp(suffix=TOMLEXT, text=True)
+        with open(filename, "w") as file:
+            toml.dump(quotes_template, file)
+        write_status = subprocess.call(
+            command.substitute(editor=editor, filename=filename), shell=True
+        )
+        if write_status != 0:
+            os.remove(filename)
+        return filename, write_status
+    else:
+        raise Exception("EDITOR not found in env")
+
+
+def insert(quotes):
+    total_quotes = len(quotes.get("-"))
+    for quote in quotes.get("-"):
+        if not quote.get("quote"):
+            console.print("[red bold]quote not added")
+            sys.exit()
+        try:
+            db.insert([
+				{
+					"quote": quote.get("quote"),
+					"author": quote.get("author") or "anonymous",
+					"reference": quote.get("reference") or "unknown",
+					"tags": quote.get("tags"),
+					"added_date": str(datetime.datetime.now()),
+				}
+			])
+        except DuplicateEntryError as e:
+            console.print("[red]Duplicate quote found")
+            sys.exit()
+    console.print(
+        "[green bold]{} {} added".format(
+            total_quotes, "quote" if total_quotes == 1 else "quotes"
+        )
+    )
+
 
 @app.command()
-def add():
-	filetype = '.toml'
-	filename, status = open_temp_toml_file()
-	default_author = 'Anonymous'
-	default_reference = 'Unknown'
-	total_quotes = 0
-	if status == 0:
-		with open(filename, 'r') as file:
-			quotes = toml.load(file)
-			total_quotes = len(quotes.get('-'))
-			for quote in quotes.get('-'):
-				if not quote.get('quote'):
-					console.print('[red bold]quote not added')
-					sys.exit()
-				db.insert(
-					[
-						{
-							"quote": quote.get('quote'),
-							"author": quote.get('author') or default_author,
-							"reference": quote.get('reference') or default_reference,
-							"tags": quote.get('tags'),
-							"added_date": str(datetime.datetime.now()),
-						}
-					]
-				)
-		db.dump()
-		console.print('[green bold]{} {} added'.format(total_quotes, 'quote' if total_quotes==1 else 'quotes'))
+def new():
+    filetype = ".toml"
+    filename, status = open_temp_toml_file()
+    total_quotes = 0
+    if status == 0:
+        with open(filename, "r") as file:
+            quotes = toml.load(file)
+            insert(quotes)
 
-@app.command("list")
-def ls(order: str, val: int = typer.Argument("10")):
+
+@app.command()
+def ls(order: str = typer.Argument("first"), val: int = typer.Argument(10)):
     if order not in ["first", "last"]:
         raise Exception('order has to be either "first" or "last"')
     all_quotes = db.find(lambda x: True)
-    ordered_latest = sorted(all_quotes, key=lambda i: i['added_date'], reverse=True) 
+    ordered_latest = sorted(all_quotes, key=lambda i: i["added_date"], reverse=True)
     if order == "first":
         for quote in all_quotes[:val]:
             display_quote(quote)
@@ -145,14 +148,11 @@ def rand():
 
 
 def init_db():
-    dbroot = pathlib.Path.home() / ".config/quote"
+    dbroot = pathlib.Path.home() / ".local/quote"
     dbroot.mkdir(parents=True, exist_ok=True)
-    DBNAME = "quotes"
-    dbpath = pathlib.Path(dbroot / "quotes.json")
-    db = jsondb.load(str(dbroot))
-    if not dbpath.exists():
-        return db.new("quotes")
-    return db.get("quotes")
+    db = jsondb(str(pathlib.Path(dbroot / "quotes.json")))
+    db.set_index("quote")
+    return db
 
 
 db = init_db()
